a16209ae3f367e017d122df3f403f4649b0081a9
Michael A Williamson <maw+github@williamsonclan.us>
2015-10-27 13:34:54 -0600

    subfooter

--- footer.php
+++ footer.php
@@ -13,11 +13,31 @@
 
 	</div><!-- #content -->
 
+
+
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'base703_s' ), 'base703_s', '<a href="http://703creative.com/" rel="designer">703 Creative</a>' ); ?>
-		</div><!-- .site-info -->
+		<div class="footer-content">
+			<div class="footer-widgets">
+&nbsp;
+			</div>
+			&nbsp;
+		</div>
+
+		<div class="subfooter">
+			<div class="subfooter-content">
+				<div class="copyright-info">
+					&copy;2015 Williamson Creative, LLC. DBA 703 Creative. All Rights Reserved.
+				</div><!-- .copyright-info -->
+
+				<div class="site-info">
+					<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'base703_s' ), 'base703_s', '<a href="http://703creative.com/" rel="designer">703 Creative</a>' ); ?>
+				</div><!-- .site-info -->
+			</div>
+		</div>
 	</footer><!-- #colophon -->
+
+
+
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
