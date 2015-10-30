752944050ce126b607ad4402b64c1a9e29f6fe01
Michael A Williamson <maw+github@williamsonclan.us>
2015-10-30 13:52:55 -0600

    Started home page for James Divine

--- home.php
+++ home.php
@@ -1,18 +1,72 @@
 <?php
 /**
- * The main template file
+ * The home page template file
  *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * e.g., it puts together the home page when no home.php file exists.
- *
- * Learn more: {@link https://codex.wordpress.org/Template_Hierarchy}
+ * This is the dedicated home page.
  *
  * @package SevenOThree
  * @package 703base
  * @since 703Base 0.1.0
  */
 
- ?>
-Home page
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+	<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="homepage" >
+	<header id="masthead" class="site-header" role="banner">
+		<div class="site-header-content">
+			<div class="site-branding">
+
+				<?php if ( get_theme_mod( 'logo' ) ) : ?>
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" id="site-logo" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+						<img src="<?php echo get_theme_mod( 'logo' ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
+					</a>
+				<?php else : ?>
+					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<?php endif; ?>
+
+			</div><!-- .site-branding -->
+
+			<nav id="site-navigation" class="main-navigation" role="navigation">
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><span></span></button>
+				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+			</nav><!-- #site-navigation -->
+		</div>
+	</header><!-- #masthead -->
+
+	<section id="hero">
+		<h1>HERO</h1>
+	</section>
+
+	<section id="current-content">
+		<h1>CURRENT-CONTENT</h1>
+	</section>
+
+	<section id="homepage-video">
+		<h1>VIDEO</h1>
+	</section>
+
+	<footer id="homepage-footer">
+		<h1>FOOTER</h1>
+		<a href="<?php echo esc_url( home_url( '/' ) ); ?>" id="site-logo" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+			<img src="<?php echo get_theme_mod( 'logo' ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
+		</a>
+	</footer>
+
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
