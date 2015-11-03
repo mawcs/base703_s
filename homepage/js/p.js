36f9059079e37fa394cd63f7a8036ff0c21131f2
Michael A Williamson <maw+github@williamsonclan.us>
2015-11-03 13:57:29 -0700

    sticky masthead

--- homepage/js/p.js
+++ homepage/js/p.js
@@ -4,8 +4,23 @@
 $(window).scroll(function(e){
     var st = $(window).scrollTop();
     phero(st);
+    stickmasthead(st);
 });
 
 function phero(st){
     $('#hero').css('top',-(st*0.25)+'px').css('opacity', (100-(st*0.15))/100);
+}
+
+function stickmasthead(st){
+    var $masthead = $("#masthead");
+    if (st < 0){
+        st = 0;
+    }
+
+    if (st === 0){
+        $masthead.removeClass("stick");
+    }
+    else{
+        $masthead.addClass("stick");
+    }
 }
